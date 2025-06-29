d3b73703902f7cb7dc2b8b9734021f126418b2a8
Subhojit Ghosh <subhojitghosh064@gmail.com>
2025-06-30 02:24:06 +0530

    feat: add ReportingPage component with initial metadata and under development message

---
+++ src/app/(protected)/reporting/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Reporting - Office Console",
+};
+
+export default async function ReportingPage() {
+  return "Under Development";
+}
