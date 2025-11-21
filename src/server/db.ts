767c2c8b44443c64231c1ed4dd10fd245536e1b3
Subhojit Ghosh <subhojit@mobotics.in>
2025-11-21 07:39:22 +0530

    chore: Remove query log level from Prisma client in development.

--- src/server/db.ts
+++ src/server/db.ts
@@ -12,7 +12,7 @@ const createPrismaClient = () => {
   return new PrismaClient({
     adapter,
     log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
   });
 };
 
